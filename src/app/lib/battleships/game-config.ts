df822972e811106fff588f2170cda3d807379728
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-16 23:11:09 +0100

    battleships: allow to specify rule how many ships has to be added

---
+++ src/app/lib/battleships/game-config.ts
@@ -0,0 +1,3 @@
+export interface GameConfig {
+  neededShips: { [key: string]: number };
+}
--- src/app/lib/battleships/game.spec.ts
+++ src/app/lib/battleships/game.spec.ts
@@ -1,10 +1,12 @@
 import { Destroyer } from './warships/destroyer';
-import { Warhsip, Coordinate } from '.';
+import { Warhsip, Coordinate, Submarine } from '.';
+import { GameConfig } from './game-config';
 
 describe('Game is not ready', () => {
   describe('When a game does not have 5 destroyer', () => {
     it('should indicate that the game is not ready', () => {
-      const game = new Game();
+      const config: GameConfig = { neededShips: { destroyer: 5 } };
+      const game = new Game(config);
       const destroyer = new Destroyer([[1, 1], [1, 2]]);
       game.addWarship(destroyer);
 
@@ -14,7 +16,8 @@ describe('Game is not ready', () => {
 
   describe('When two warships have a same coordinate', () => {
     it('should fail', () => {
-      const game = new Game();
+      const config: GameConfig = { neededShips: { destroyer: 2 } };
+      const game = new Game(config);
       const destroyer = new Destroyer([[1, 1], [1, 2]]);
 
       game.addWarship(destroyer);
@@ -25,9 +28,10 @@ describe('Game is not ready', () => {
 });
 
 describe('Game is ready to play', () => {
-  describe('When a game has all needed warships', () => {
+  describe('When a game has all needed destroyers', () => {
     it('should be ready', () => {
-      const game = new Game();
+      const config: GameConfig = { neededShips: { destroyer: 2 } };
+      const game = new Game(config);
       const destroyer_1 = new Destroyer([[1, 1], [1, 2]]);
       const destroyer_2 = new Destroyer([[3, 3], [3, 4]]);
 
@@ -37,16 +41,35 @@ describe('Game is ready to play', () => {
       expect(game.isReadyToPlay).toBe(true);
     });
   });
+
+  describe('When a game has all needed submarines and destroyers', () => {
+    it('should be ready', () => {
+      const config: GameConfig = {
+        neededShips: { destroyer: 2, submarine: 3 }
+      };
+
+      const game = new Game(config);
+
+      const warships = [
+        new Destroyer([[1, 1], [1, 2]]),
+        new Destroyer([[2, 1], [2, 2]]),
+        new Submarine([[1, 3], [1, 4], [1, 5]]),
+        new Submarine([[2, 3], [2, 4], [2, 5]]),
+        new Submarine([[3, 3], [3, 4], [3, 5]])
+      ];
+
+      warships.forEach(warship => game.addWarship(warship));
+
+      expect(game.isReadyToPlay).toBe(true);
+    });
+  });
 });
 
 export class Game {
-  neededShips = {
-    destroyer: 2
-  };
-
+  private _warships: Warhsip[] = [];
   isReadyToPlay = false;
 
-  constructor(private _warships: Warhsip[] = []) {}
+  constructor(private _config: GameConfig) {}
 
   addWarship(warship: Destroyer) {
     this._throwIfCoordinatesAreAlreadyTaken(warship.coordinates);
@@ -70,7 +93,7 @@ export class Game {
   }
 
   private _updateReadiness(): void {
-    this.isReadyToPlay = Object.keys(this.neededShips).every(shipName =>
+    this.isReadyToPlay = Object.keys(this._config.neededShips).every(shipName =>
       this._amountOfShipIsSatisfied(shipName)
     );
   }
@@ -80,7 +103,7 @@ export class Game {
       this._warships
         .map(warship => warship.name)
         .filter(name => name.toLowerCase() === shipName.toLowerCase())
-        .length === this.neededShips[shipName]
+        .length === this._config.neededShips[shipName]
     );
   }
 }
