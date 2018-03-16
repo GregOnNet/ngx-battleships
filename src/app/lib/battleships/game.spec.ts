f4f9ec5585ce1c021aa8d1c517f8833c5ceed5aa
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-03-16 23:15:44 +0100

    battleshop: move game into own file

--- src/app/lib/battleships/game.spec.ts
+++ src/app/lib/battleships/game.spec.ts
@@ -1,6 +1,7 @@
-import { Destroyer } from './warships/destroyer';
-import { Warhsip, Coordinate, Submarine } from '.';
+import { Submarine } from '.';
+import { Game } from './game';
 import { GameConfig } from './game-config';
+import { Destroyer } from './warships/destroyer';
 
 describe('Game is not ready', () => {
   describe('When a game does not have 5 destroyer', () => {
@@ -64,46 +65,3 @@ describe('Game is ready to play', () => {
     });
   });
 });
-
-export class Game {
-  private _warships: Warhsip[] = [];
-  isReadyToPlay = false;
-
-  constructor(private _config: GameConfig) {}
-
-  addWarship(warship: Destroyer) {
-    this._throwIfCoordinatesAreAlreadyTaken(warship.coordinates);
-
-    this._warships = [...this._warships, warship];
-    this._updateReadiness();
-  }
-
-  private _throwIfCoordinatesAreAlreadyTaken(coordinates: Coordinate[]): void {
-    const takenCoordinate = this._warships
-      .map(warship => warship.coordinates)
-      .reduce((prev, curr) => prev.concat(curr), [])
-      .find(coordinate => coordinates.some(c => c === coordinate));
-
-    if (takenCoordinate) {
-      throw new Error(
-        `Sorry, but the field [${takenCoordinate.x}, ${takenCoordinate.y}]` +
-          'is already taken.'
-      );
-    }
-  }
-
-  private _updateReadiness(): void {
-    this.isReadyToPlay = Object.keys(this._config.neededShips).every(shipName =>
-      this._amountOfShipIsSatisfied(shipName)
-    );
-  }
-
-  private _amountOfShipIsSatisfied(shipName: string) {
-    return (
-      this._warships
-        .map(warship => warship.name)
-        .filter(name => name.toLowerCase() === shipName.toLowerCase())
-        .length === this._config.neededShips[shipName]
-    );
-  }
-}
--- src/app/lib/battleships/index.ts
+++ src/app/lib/battleships/index.ts
@@ -1,7 +1,9 @@
+export * from './coordinate';
+export * from './game';
+
 export * from './warships/battleship';
 export * from './warships/carrier';
 export * from './warships/cruiser';
-export * from './coordinate';
 export * from './warships/destroyer';
 export * from './warships/submarine';
 export * from './warships/warship';
